f54833339a28561334191fa19a9dd7eb60d2b1e1
akhil-rane <akhil131192@gmail.com>
2019-06-04 10:05:32 -0400

    deleting webhook.sh

--- utils/webhook.sh
+++
@@ -1,8 +0,0 @@
-curl -X POST -H "Content-Type: application/json" \
-https://api.trello.com/1/tokens/e295dcc624b627e4e0250c93c8c9a7b73d5b6640c4f65a5d40f30f5984acba13/webhooks/ \
--d '{
-  "key": "fe4f115b8ed7978d6027514f094a79df",
-  "callbackURL": "http://5c074ae1.ngrok.io/trello",
-  "idModel":"5cf1917d5442a621eb6b3891",
-  "description": "trello webhook"  
-}'
