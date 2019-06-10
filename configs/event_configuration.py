61c1e58b9cbd672a7eebe47818b1610a9cae8bad
akhil-rane <akhil131192@gmail.com>
2019-06-10 17:58:14 -0400

    initial event configuration file that stores configs of an event

---
+++ configs/event_configuration.py
@@ -0,0 +1,11 @@
+event_configuration = {
+    "pull_request" : {
+        "filter" : "filters/pull_request_filter.json",
+        "formatter" : "formatters/pull_request_formmatter.json"
+    },
+
+    "issue" : {
+        "filter" : "filters/issue_filter.json",
+        "formatter" : "formatters/issue_formmatter.json"
+    }
+}
