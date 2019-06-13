90374a28f3da43af00e6aa45860902186344fd3c
akhil-rane <akhil131192@gmail.com>
2019-06-13 13:54:41 -0400

    Initial badge configuration file

---
+++ configs/badge_configuration.py
@@ -0,0 +1,38 @@
+badges = {
+    'first_pull': {
+        'description': 'first pull request merged',
+        'event_type': 'pull_request',
+        'criteria': 1,
+        'image_file': None
+    },
+    'pull_master' : {
+        'description': '10 pull requests merged',
+        'event_type': 'pull_request',
+        'criteria': 10,
+        'image_file': None
+    },
+    'first_issue': {
+        'description': 'first issue created',
+        'event_type': 'issue',
+        'criteria': 1,
+        'image_file': None
+    },
+    'issue_finder': {
+        'description': '10 issues created',
+        'event_type': 'issue',
+        'criteria': 5,
+        'image_file': None
+    },
+    'first_comment': {
+        'description': 'first comment',
+        'event_type': 'comment',
+        'criteria': 1,
+        'image_file': None
+    },
+    'team_player': {
+        'description': 'awarded for 5 comments',
+        'event_type': 'comment',
+        'criteria': 5,
+        'image_file': None
+    }
+}
