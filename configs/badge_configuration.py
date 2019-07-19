a66a45dd7ef6f62ae3e2ebe3ff4d364f589168ef
akhil-rane <akhil131192@gmail.com>
2019-07-19 18:50:58 -0400

    Added new badges

--- configs/badge_configuration.py
+++ configs/badge_configuration.py
@@ -1,5 +1,5 @@
 badges = {
-    'first_pull': {
+    'first-pull': {
         'description': 'first pull request merged',
         'event_type': 'pull_request',
         'criteria': {
@@ -8,7 +8,16 @@ badges = {
         },
         'image_file': None
     },
-    'pull_master' : {
+    'pull-master' : {
+        'description': '5 pull requests merged',
+        'event_type': 'pull_request',
+        'criteria': {
+            'type': 'count',
+            'value': 5
+        },
+        'image_file': None
+    },
+    'pull-king' : {
         'description': '10 pull requests merged',
         'event_type': 'pull_request',
         'criteria': {
@@ -17,7 +26,7 @@ badges = {
         },
         'image_file': None
     },
-    'first_issue': {
+    'first-issue': {
         'description': 'awarded for creating first issue',
         'event_type': 'issue',
         'criteria': {
@@ -26,39 +35,57 @@ badges = {
         },
         'image_file': None
     },
-    'issue_finder': {
-        'description': 'awarded for creating 5 issues',
-        'event_type': 'issue',
-        'criteria': {
-            'type': 'count',
-            'value': 5
-        },
-        'image_file': None
-    },
-    'first_comment': {
-        'description': 'awarded for first comment',
-        'event_type': 'comment',
+    'first-github-comment': {
+        'description': 'awarded for first github comment',
+        'event_type': 'github_comment',
         'criteria': {
             'type': 'count',
             'value': 1
         },
         'image_file': None
     },
-    'team_player': {
-        'description': 'awarded for 5 comments',
-        'event_type': 'comment',
+    'first-bug-fix': {
+        'description': 'awarded for fixing the first issue with tag bug',
+        'event_type': 'bug_fix',
+        'criteria': {
+            'type': 'count',
+            'value': 1
+        },
+        'image_file': None
+    },
+    'bug-squasher': {
+        'description': 'awarded for fixing five issues with tag bug',
+        'event_type': 'bug_fix',
         'criteria': {
             'type': 'count',
             'value': 5
         },
         'image_file': None
     },
-    'first_trello_idea': {
-        'description': 'awarded for creating first trello card in New list',
-        'event_type': 'new_idea',
+    'first-review': {
+        'description': 'awarded for reviewing and merging first pull-request',
+        'event_type': 'review',
         'criteria': {
             'type': 'count',
             'value': 1
+        },
+        'image_file': None
+    },
+    'review-master' : {
+        'description': 'awarded for reviewing and merging 5 pull-requests',
+        'event_type': 'review',
+        'criteria': {
+            'type': 'count',
+            'value': 5
+        },
+        'image_file': None
+    },
+    'review-king' : {
+        'description': 'awarded for reviewing and merging 10 pull-requests',
+        'event_type': 'review',
+        'criteria': {
+            'type': 'count',
+            'value': 10
         },
         'image_file': None
     },
@@ -79,6 +106,66 @@ badges = {
                     'event_type': 'issue',
                     'field': 'raw_github.issue.number'
                 }
+            ]
+        },
+        'image_file': None
+    },
+    'first-trello-comment': {
+        'description': 'awarded for first trello comment',
+        'event_type': 'trello_comment',
+        'criteria': {
+            'type': 'count',
+            'value': 1
+        },
+        'image_file': None
+    },
+    'first_idea': {
+        'description': 'awarded for creating first trello card in New list',
+        'event_type': 'new_idea',
+        'criteria': {
+            'type': 'count',
+            'value': 1
+        },
+        'image_file': None
+    },
+    'think-tank': {
+        'description': 'awarded for creating 5 trello cards in New list',
+        'event_type': 'new_idea',
+        'criteria': {
+            'type': 'count',
+            'value': 5
+        },
+        'image_file': None
+    },
+    'finisher': {
+        'description': 'awarded for moving a card in the completed list',
+        'event_type': 'task_completed',
+        'criteria': {
+            'type': 'every_event'
+        },
+        'image_file': None
+    },
+    'trellorun': {
+        'description': 'awarded for creating a card in new list, moving it through backlog and in-progress lists and finally in completed list',
+        'criteria': {
+            'type': 'match',
+            'matching_events': [
+                {
+                    'event_type': 'new_idea',
+                    'field': 'raw_trello.action.data.card.id'
+                },
+                {
+                    'event_type': 'task_moved_to_backlog',
+                    'field': 'raw_trello.action.data.card.id'
+                },
+                {
+                    'event_type': 'task_moved_to_in-progress',
+                    'field': 'raw_trello.action.data.card.id'
+                },
+                {
+                    'event_type': 'task_completed',
+                    'field': 'raw_trello.action.data.card.id'
+                },
             ]
         },
         'image_file': None
